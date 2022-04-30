2cd64b64eedea54fafc1864bbdf2c50c58b6671f
zhucj <zhucj@servyou.com.cn>
2022-04-30 14:55:43 +0800

    idplugin-idea-base

--- idplugin-idea-base/src/main/java/com/janita/idplugin/idea/base/NotificationUtils.java
+++ idplugin-idea-base/src/main/java/com/janita/idplugin/idea/base/NotificationUtils.java
@@ -1,4 +1,4 @@
-package com.janita.idplugin.common.idea;
+package com.janita.idplugin.idea.base;
 
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationDisplayType;
--- idplugin-woodpecker/src/main/java/com/janita/idplugin/woodpecker/TestAction.java
+++ idplugin-woodpecker/src/main/java/com/janita/idplugin/woodpecker/TestAction.java
@@ -1,13 +1,9 @@
 package com.janita.idplugin.woodpecker;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationDisplayType;
-import com.intellij.notification.NotificationGroup;
-import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.MessageType;
-import com.janita.idplugin.remote.RemoteApplication;
+import com.janita.idplugin.idea.base.NotificationUtils;
 
 /**
  * TestAction
@@ -19,11 +15,6 @@ public class TestAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-
-        RemoteApplication.main(null);
-
-        NotificationGroup notificationGroup = new NotificationGroup("Code Review", NotificationDisplayType.BALLOON, true);
-        Notification notification = notificationGroup.createNotification("content", MessageType.INFO);
-        notification.setTitle("Code review");
-        Notifications.Bus.notify(notification);    }
+        NotificationUtils.showNotification("你好", MessageType.ERROR);
+    }
 }
