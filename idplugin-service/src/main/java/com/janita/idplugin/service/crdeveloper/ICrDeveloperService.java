ef66464dd864398a5c8cd1a9a71879126b2c69f0
zhucj <zhucj@servyou.com.cn>
2022-05-01 16:42:15 +0800

    woodpecker - 添加注释

--- idplugin-service/src/main/java/com/janita/idplugin/service/crdeveloper/ICrDeveloperService.java
+++ idplugin-service/src/main/java/com/janita/idplugin/service/crdeveloper/ICrDeveloperService.java
@@ -16,9 +16,29 @@ import java.util.List;
  */
 public interface ICrDeveloperService {
 
+    /**
+     * 保存
+     *
+     * @param setting 配置
+     * @param request 参数
+     */
     void save(CrQuestionSetting setting, CrDeveloperSaveRequest request);
 
+    /**
+     * 查询开发列表
+     *
+     * @param setting 配置
+     * @param request 参数
+     * @return 列表
+     */
     List<CrDeveloper> query(CrQuestionSetting setting, CrDeveloperQueryRequest request);
 
+    /**
+     * 根据项目名称查询
+     *
+     * @param setting 配置
+     * @param projectName 项目名称
+     * @return 开发列表
+     */
     Pair<Boolean, List<CrDeveloper>> queryAssignName(CrQuestionSetting setting, String projectName);
 }
