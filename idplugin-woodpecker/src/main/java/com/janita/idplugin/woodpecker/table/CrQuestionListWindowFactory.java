d2c76a313ee07fd6a8e6e729f3b0586f9eeb230d
zhucj <zhucj@servyou.com.cn>
2022-05-01 16:50:06 +0800

    woodpecker - 添加注释

--- idplugin-woodpecker/src/main/java/com/janita/idplugin/woodpecker/table/CrQuestionListWindowFactory.java
+++ idplugin-woodpecker/src/main/java/com/janita/idplugin/woodpecker/table/CrQuestionListWindowFactory.java
@@ -2,9 +2,7 @@ package com.janita.idplugin.woodpecker.table;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.openapi.wm.ToolWindowType;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
@@ -22,7 +20,7 @@ public class CrQuestionListWindowFactory implements ToolWindowFactory {
         CrQuestionListWindow window = new CrQuestionListWindow(project, toolWindow);
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent((JComponent) window.getContentPane(), "", false);
-        toolWindow.setDefaultState(ToolWindowAnchor.RIGHT, ToolWindowType.WINDOWED, null);
+        // toolWindow.setDefaultState(ToolWindowAnchor.RIGHT, ToolWindowType.WINDOWED, null);
         toolWindow.getContentManager().addContent(content);
     }
 }
